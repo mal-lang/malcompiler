6fa5d078b71b0d8dbd72263ff7edc47b3b8c9281
Max Wällstedt <max.wallstedt@foreseeti.com>
2019-10-25 13:46:41 +0200

    Remove echo from malc.bat

--- malcompiler-jlink/src/main/resources/malc.bat
+++ malcompiler-jlink/src/main/resources/malc.bat
@@ -5,7 +5,7 @@ set BIN_DIR=%BIN_DIR:~0,-1%
 for %%F in ("%BIN_DIR%") do set RUNTIME_DIR=%%~dpF
 set RUNTIME_DIR=%RUNTIME_DIR:~0,-1%
 
-echo "%BIN_DIR%\java" ^
+"%BIN_DIR%\java" ^
   --module-path "%RUNTIME_DIR%\dependencies" ^
   --module "org.mal_lang.compiler.cli" ^
   %*
